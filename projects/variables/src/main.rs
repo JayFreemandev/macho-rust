9cd85b17be3bf42e4211f951115c83d706bba50e
jaeyun <jayfreemandev@gmail.com>
2025-04-16 10:37:38 +0900

    Mutablity의 차이를 이해하라

---
+++ projects/variables/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let x = 5;
+    println!("The value of x is: {x}");
+    let x =  x + 1;
+    let x = x + 1;
+    println!("The value of x is: {x}");
+
+    // let spaces = " ";
+    // let spaces = spaces.len();
+
+
+    let mut spaces = "  ";
+    spaces = spaces.len();
+    
+}
